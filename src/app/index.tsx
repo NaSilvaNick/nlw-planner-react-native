da3c3ac7870c73931bc39a226516f9c326945a3f
NaSilvaNick <nicklops8@gmail.com>
2024-07-09 21:53:30 -0300

    Initial Config

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Index() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='font-bold text-xl text-zinc-200'>Convide seus amigos e planeje sua proxima viagem!</Text>
+    </View>
+  )
+}
