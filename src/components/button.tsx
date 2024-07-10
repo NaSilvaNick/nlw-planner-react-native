23fb18624a06072aadc70f34b14a5f8de0a4c1a7
NaSilvaNick <nicklops8@gmail.com>
2024-07-10 11:32:40 -0300

    feat: UI initial screen

---
+++ src/components/button.tsx
@@ -0,0 +1,47 @@
+import clsx from "clsx";
+import { createContext, useContext } from "react";
+import { ActivityIndicator, Text, TextProps, TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+type Variants = 'primary' | 'secondary'
+
+type ButtonProps = TouchableOpacityProps & {
+  variant?: Variants,
+  isLoading?: boolean
+}
+
+const ThemeContext = createContext<{ variant?: Variants}>({})
+
+function Button({ variant = 'primary', isLoading = false, children, ...rest } : ButtonProps) {
+  return (
+    <TouchableOpacity
+      {...rest}
+      disabled={isLoading}
+      activeOpacity={0.7}
+      className={clsx("w-full h-11 flex-row items-center justify-center rounded-lg gap-2",{
+        "bg-lime-300": variant === 'primary',
+        "bg-zinc-800": variant === 'secondary'
+      })}
+    >
+      <ThemeContext.Provider value={{variant}}>
+        {isLoading ? <ActivityIndicator className="text-lime-950" /> : children}
+      </ThemeContext.Provider>
+    </TouchableOpacity>
+  )
+}
+
+function Title(props : TextProps) {
+  const { variant } = useContext(ThemeContext)
+  return (
+    <Text
+      {...props}
+      className={clsx("text-base font-semibold",{
+        "text-lime-950": variant === 'primary',
+        "text-zinc-200": variant === 'secondary'
+      })}
+    />
+  )
+}
+
+Button.Title = Title
+
+export { Button }
